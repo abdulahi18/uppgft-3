3492b08eb84ad48be864e5b200911c62bf35d874
Abdulahi18 <abib05001@utb.vaxjo.se>
2023-09-20 10:31:36 +0200

    Add project files.

---
+++ uppgft 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Abdullahi
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            int startavgift = 300;
+
+            int kostnadPerKilometer = 1;
+
+            int kostnadPerExtraDag = 500;
+
+            
+            
+            Console.WriteLine("hur många dagar vill du hyra bilen?");
+            int antalDagar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("hur många kilometer ska du köra?");
+            double antalKilometer = double.Parse(Console.Realine());
+
+            int totalhyra = startavgift + (antalDagar - 1) * kostnadPerExtraDag + (int) (antalKilometer * kostnadPerKilometer);
+
+            Console.WriteLine($"den totala hyran blir: {totalhyra} kr");
+        }
+    }
+}
